0cac371cbc133189ec953f3a386f276e0e8e27aa
nguyentrongnghiaitnlu <67213190+nguyentrongnghiaitnlu@users.noreply.github.com>
2020-07-26 11:34:34 +0700

    Có thêm thư viện vô á tài :V
    ố kề done nha

--- src/getdata/ChilkatSCP.java
+++ src/getdata/ChilkatSCP.java
@@ -1,9 +1,12 @@
 ﻿package getdata;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +16,24 @@ import com.chilkatsoft.CkSsh;
 
 import model.Configuration;
 import model.ConnectDatabase;
+import model.GetConnection;
+import model.SendMail;
 
 public class ChilkatSCP {
-
+	SendMail sm = new SendMail();
+	private PreparedStatement pst = null;
+	private ResultSet rs = null;
+	private String sql;
+	private Connection conn = null;
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	LocalDateTime now = LocalDateTime.now();
+	String timestamp = dtf.format(now);
+	static final String NUMBER_REGEX = "^[0-9]+$";
+	String server_path;
+	String local_path;
+	private String sqlCheck;
+	private PreparedStatement pstCheck = null;
+	private ResultSet rsCheck;
 	static {
 		try {
 			System.loadLibrary("chilkat"); // copy file chilkat.dll vao thu muc project
@@ -38,67 +56,200 @@ public class ChilkatSCP {
 		Connection connection = cdb.connectDBControl();
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		ResultSet resultSet = preparedStatement.executeQuery();
-		
-		while(resultSet.next()) {
+
+		while (resultSet.next()) {
 			configuration.setHost_name(resultSet.getString(1));
 			configuration.setPort(resultSet.getInt(2));
 			configuration.setUser_name(resultSet.getString(3));
 			configuration.setPassword(resultSet.getString(4));
-			
+
 			list.add(configuration);
 		}
 		return list;
 	}
 
-	public void downloadSCP(Configuration configuration) {
+	public boolean chilkatSCPDownLoad(Configuration config) {
 		CkSsh ssh = new CkSsh();
 		CkGlobal ck = new CkGlobal();
-		ck.UnlockBundle("Hello team14");
-		String hostname = configuration.getHost_name();
-		int port = configuration.getPort();
-		boolean success = ssh.Connect(hostname, port);
+		ck.UnlockBundle("Hello Team14");
+		// 1.1. Kiểm tra kết nối đến address server thành công hay chưa?
+		boolean success = ssh.Connect(config.getHost_name(), config.getPort());
 		if (success != true) {
-			System.out.println(ssh.lastErrorText());
-			return;
+
+			// 1.1.1. sendMail error hostname or port
+			sm.sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Server không tìm thấy...!!");
+			return false;
 		}
 
 		ssh.put_IdleTimeoutMs(5000);
-		success = ssh.AuthenticatePw(configuration.getUser_name(), configuration.getPassword());
+		// 1.2. Kiểm tra tài khoản và mặt khẩu kết nối đến address server thành công hay
+		// chưa?
+		success = ssh.AuthenticatePw(config.getUser_name(), config.getPassword());
 		if (success != true) {
-			System.out.println(ssh.lastErrorText());
-			return;
+			// 1.2.1. sendmail error username or password
+			sm.sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Tài khoản hoặc mặt khẩu sai!...");
+			return false;
 		}
 		CkScp scp = new CkScp();
-
+		// 1.3. Kiểm tra sử dụng SSH với tài khoản và mặt khẩu kết nối đến address
+		// server thành công hay chưa?
 		success = scp.UseSsh(ssh);
 		if (success != true) {
-			System.out.println(scp.lastErrorText());
-			return;
+			// 1.3.1. sendMail error using ssh
+			sm.sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Không có kết nối!...");
+			return false;
 		}
-		scp.put_SyncMustMatch("sinhvien*.*");// down tat ca cac file bat dau bang sinhvien
-//		String remotePath = "/volume1/ECEP/song.nguyen/DW_2020/data";
-		String remotePath = "/ECEP/song.nguyen/DW_2020/data";
-		String localPath = "G:\\CodeJava\\Data"; // thu muc muon down file ve
-		success = scp.SyncTreeDownload(remotePath, localPath, 2, false);
+		scp.put_SyncMustMatch(config.getFile_name());
+//		scp.put_SyncMustNotMatch(synMustMath);
+		// không tải những file đã tải rồi
+		success = scp.SyncTreeDownload(config.getServer_dir(), config.getImport_dir(), config.getMode_scp(), false);
+		// 1.4. Kiểm tra đã tải được file hay chưa
 		if (success != true) {
-			System.out.println(scp.lastErrorText());
-			return;
+			// 1.4.1. sendMail error download fail
+			sm.sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE",
+					"Thư mực server hoặc địa chỉ local không tìm thấy!...");
+			return false;
+		}
+		// 1.4.2. sendMail download success
+		sm.sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Đã tải file thành công!...");
+		ssh.Disconnect();
+		return true;
+	}
+
+	public boolean isDownLoadSCPChilkat(int id) {
+		boolean result = false;
+		sql = "SELECT * FROM configuration where config_id = '" + id + "'";
+		try {
+			// 1. select fields table SCP in data SCP using download
+			pst = new GetConnection().getConnection("controldb").prepareStatement(sql);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				int id_scp = rs.getInt("config_id");
+				String load_library = rs.getString("load_library");
+				String host_name_scp = rs.getString("host_name_scp");
+				int port_scp = rs.getInt("port_scp");
+				String username_scp = rs.getString("username_scp");
+				String password_scp = rs.getString("password_scp");
+				String sync_must_math = rs.getString("file_name");
+				server_path = rs.getString("server_dir");
+				local_path = rs.getString("import_dir");
+				int mode_scp = rs.getInt("mode_scp");
+				Configuration scp_config = new Configuration(id_scp, load_library, host_name_scp, port_scp,
+						username_scp, password_scp, sync_must_math, server_path, local_path, mode_scp);
+
+				if (chilkatSCPDownLoad(scp_config)) {
+					result = true;
+					return result;
+				} else {
+					return result;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return result;
+
+		} finally {
+			try {
+				if (pst != null)
+					pst.close();
+				if (rs != null)
+					rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
 
-		ssh.Disconnect();
+		return result;
 	}
-	
+
+	public boolean insertDataLog(int id) {
+		int rs = 0;
+		boolean check = false;
+		if (!isDownLoadSCPChilkat(id)) {
+			return check;
+		} else {
+			sql = "INSERT INTO log (file_name,data_file_config_id,file_status,staging_load_count, timestamp_download, timestamp_insert_staging, timestamp_insert_datawarehouse)"
+					+ " values (?,?,?,?,?,?,?)";
+			File localPath = new File(local_path);
+			File[] listFileLog = localPath.listFiles();
+
+			lable: for (int i = 0; i < listFileLog.length; i++) {
+				String sqlCheck = "select * from log";
+				try {
+					pstCheck = new GetConnection().getConnection("controldb").prepareStatement(sqlCheck);
+					rsCheck = pstCheck.executeQuery();
+					while (rsCheck.next()) {
+						if (rsCheck.getString(2).equals(listFileLog[i].getName())) {
+							continue lable;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					return check;
+				} finally {
+					try {
+						if (pstCheck != null)
+							pstCheck.close();
+						if (this.rsCheck != null)
+							this.rsCheck.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				}
+
+				try {
+					pst = new GetConnection().getConnection("controldb").prepareStatement(sql);
+					pst.setString(1, listFileLog[i].getName());
+//								System.out.println(listFileLog[i].getName());
+					pst.setInt(2, id);
+					pst.setString(3, "ER");
+					pst.setInt(4, 0);
+					pst.setString(5, timestamp);
+					pst.setString(6, null);
+					pst.setString(7, null);
+					rs = pst.executeUpdate();
+					check = true;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return check;
+				} finally {
+					try {
+						if (pst != null)
+							pst.close();
+						if (this.rs != null)
+							this.rs.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+		}
+
+		return check;
+
+	}
+	public void sendMailInsertLog(int id) {
+		if (insertDataLog(id) == true) {
+			// 1.6.1 if success, sendmail with content success
+//			sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Ghi log thành công rồi bồ tèo...!");
+			System.out.println("Send mail write log success!!!");
+		} else {
+			// 1.6.2 if fail, sendmail with content fail
+//			sendMail("17130135@st.hcmuaf.edu.vn", "DATA WAREHOUSE", "Ghi log fail rồi bồ tèo...!");
+			System.out.println("Send mail write log fail!!!");
+
+		}
+	}
 	public static void main(String[] args) throws SQLException {
-//		ChilkatSCP chilkatSCP = new ChilkatSCP();
-//
-//		List<Configuration> configs = chilkatSCP.getSrc();
-//		for (Configuration configuration : configs) {
-//			chilkatSCP.downloadSCP(configuration);
-//		}
-		CkGlobal ckGlobal = new CkGlobal();
-		ckGlobal.UnlockBundle("abc");
-		CkSsh ckSsh = new CkSsh();
-		System.out.println(ckSsh != null);
-		
+		ChilkatSCP test = new ChilkatSCP();
+//		Configuration config = new Configuration(1, "chilkat", "drive.ecepvn.org", 2227, "guest_access", "123456", "sinhvien*.xlsx", "/volume1/ECEP/song.nguyen/DW_2020/data", "D:\\HK6 2019 - 2020\\Data WAREHOUSE\\DIR\\Student", 6);
+//		test.chilkatSCPDownLoad(config);
+//		test.isDownLoadSCPChilkat(1);
+		test.insertDataLog(1);
+//		test.sendMailInsertLog(1);
 	}
 }
--- src/model/Configuration.java
+++ src/model/Configuration.java
@@ -4,16 +4,20 @@ public class Configuration {
 //	host_name, port, user_name, password, file_name, file_type, import_dir, success_dir, error_dir, column_list, variable_list, delimeter
 	
 	private int config_id;
+	private String load_library;
 	private String host_name;
 	private int port;
 	private String user_name;
 	private String password;
 	private String file_name;
 	private String file_type;
+	private String server_dir;
 	private String import_dir;
+	private int mode_scp;
 	private String success_dir;
 	private String error_dir;
 	private String column_list;
+	private String variable_list;
 	private String delimeter;
 	
 	public Configuration() {
@@ -35,6 +39,21 @@ public class Configuration {
 		this.error_dir = error_dir;
 		this.column_list = column_list;
 		this.delimeter = delimeter;
+	}
+	
+	public Configuration(int config_id, String load_library, String host_name, int port, String user_name,
+			String password, String file_name, String server_dir, String import_dir, int mode_scp) {
+		super();
+		this.config_id = config_id;
+		this.load_library = load_library;
+		this.host_name = host_name;
+		this.port = port;
+		this.user_name = user_name;
+		this.password = password;
+		this.file_name = file_name;
+		this.server_dir = server_dir;
+		this.import_dir = import_dir;
+		this.mode_scp = mode_scp;
 	}
 	public int getConfig_id() {
 		return config_id;
@@ -107,5 +126,29 @@ public class Configuration {
 	}
 	public void setDelimeter(String delimeter) {
 		this.delimeter = delimeter;
+	}
+	public String getLoad_library() {
+		return load_library;
+	}
+	public void setLoad_library(String load_library) {
+		this.load_library = load_library;
+	}
+	public String getServer_dir() {
+		return server_dir;
+	}
+	public void setServer_dir(String server_dir) {
+		this.server_dir = server_dir;
+	}
+	public int getMode_scp() {
+		return mode_scp;
+	}
+	public void setMode_scp(int mode_scp) {
+		this.mode_scp = mode_scp;
+	}
+	public String getVariable_list() {
+		return variable_list;
+	}
+	public void setVariable_list(String variable_list) {
+		this.variable_list = variable_list;
 	}
 }
